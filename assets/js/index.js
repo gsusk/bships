d9ae543d32c0fa10ef82c7ad383cdb4b3d48dac6
gsus <satehortuamercado@gmail.com>
2023-10-30 15:13:05 -0500

    game over add

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,7 +6,7 @@ let board = new Board(); // creates a new game board
 // Create the UI of the game using HTML elements based on this grid.
 
 // Your code here
-
+let game = false;
 console.log(board.grid);
 
 const body = document.body
@@ -15,8 +15,13 @@ let div = document.createElement("div");
 body.appendChild(div)
 div.classList.add("center")
 div.classList.add('div-style')
-
-
+let win = document.createElement("div")
+let h2 = document.createElement("h2")
+body.insertBefore(win, div)
+h2.classList.add("hidden")
+h2.classList.add("shown")
+h2.innerText = " YOU WIN! "
+win.appendChild(h2)
 
 board.grid.forEach((row, idx) => {
 
@@ -26,14 +31,27 @@ board.grid.forEach((row, idx) => {
     innerDiv.setAttribute("data-row", `${idx}`)
     innerDiv.setAttribute("data-column", `${idy}`)
 
-    let value = div.appendChild(innerDiv)
-    value.innerText = cell
-    value.addEventListener('click', (e) => {
+    let content = div.appendChild(innerDiv)
+    content.innerText = cell
+    content.addEventListener('click', (e) => {
       const [row, column] = [innerDiv.dataset.row, innerDiv.dataset.column];
-      console.log(row, column);
-      board.makeHit(row, column)
-      console.log(e.target)
+      if (innerDiv.style.backgroundColor === "green" || game) {
+        return
+      }
 
+      let hit = board.makeHit(row, column);
+
+      if (typeof hit === "number") {
+        innerDiv.style.backgroundColor = "green";
+      } else {
+        innerDiv.style.backgroundColor = "red";
+      }
+
+      if (board.isGameOver()) {
+        let all = document.querySelectorAll(".inner-div")
+        game = true;
+        h2.classList.remove("hidden")
+      }
     })
   })
 
