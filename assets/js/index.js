5a50bf5f91faf02e1290329149f0f3c08fae9f53
gsus <satehortuamercado@gmail.com>
2023-10-30 13:50:31 -0500

    initial commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,4 +6,18 @@ let board = new Board(); // creates a new game board
 // Create the UI of the game using HTML elements based on this grid.
 console.log(board.grid);
 
+const body = document.body
+body.classList.add("center")
+let div = document.createElement("div");
+body.appendChild(div)
+div.classList.add("center")
+div.classList.add('div-style')
+
+board.grid.forEach((row) => {
+  row.forEach((cell) => {
+    let innerDiv = document.createElement('div');
+    innerDiv.classList.add("inner-div")
+    div.appendChild(innerDiv)
+  })
+})
 // Your code here
