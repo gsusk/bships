7864db17ce234aea9ccd23ac6386e1d1051ccf55
gsus <satehortuamercado@gmail.com>
2023-10-30 14:16:46 -0500

    added event for hit

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,9 @@ let board = new Board(); // creates a new game board
 
 // Examine the grid of the game board in the browser console.
 // Create the UI of the game using HTML elements based on this grid.
+
+// Your code here
+
 console.log(board.grid);
 
 const body = document.body
@@ -13,11 +16,25 @@ body.appendChild(div)
 div.classList.add("center")
 div.classList.add('div-style')
 
-board.grid.forEach((row) => {
-  row.forEach((cell) => {
+
+
+board.grid.forEach((row, idx) => {
+
+  row.forEach((cell, idy) => {
     let innerDiv = document.createElement('div');
     innerDiv.classList.add("inner-div")
-    div.appendChild(innerDiv)
+    innerDiv.setAttribute("data-row", `${idx}`)
+    innerDiv.setAttribute("data-column", `${idy}`)
+
+    let value = div.appendChild(innerDiv)
+    value.innerText = cell
+    value.addEventListener('click', (e) => {
+      const [row, column] = [innerDiv.dataset.row, innerDiv.dataset.column];
+      console.log(row, column);
+      board.makeHit(row, column)
+      console.log(e.target)
+
+    })
   })
+
 })
-// Your code here
